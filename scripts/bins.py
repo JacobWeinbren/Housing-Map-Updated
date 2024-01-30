32324869f56aa203f64d9b3fec03c1dc2baffec5
Jacob Weinbren <jacobweinbren@gmail.com>
2024-01-30 21:46:14 +0000

    Added to Github Pages

--- scripts/bins.py
+++ scripts/bins.py
@@ -5,10 +5,21 @@ import json
 # Load the data
 df = pd.read_csv("output/combined_postcode_sales.csv")
 
-# Group by year and calculate the median and percentiles
+# Ensure price_normalized is within 0-100
+df["price_normalized"] = df["price_normalized"].clip(lower=0, upper=100)
+
+# Define bins edges
+bins_edges = np.arange(0, 51, 2)  # 0, 10, 20, ..., 100
+
+
+# Group by year and calculate the histogram for each group
+def calculate_histogram(x):
+    hist, _ = np.histogram(x, bins=bins_edges)
+    return hist.tolist()
+
+
 grouped = df.groupby("year")["price_normalized"].agg(
-    median="median",
-    bins=lambda x: [round(p, 2) for p in np.percentile(x, np.linspace(0, 100, 11))],
+    median="median", histogram=calculate_histogram
 )
 
 # Round the median to 2 decimal places
